61f858c968ed31ddc11a2693cc98578ab58d4677
fumengyao5544 <fumengyao5544@gmail.com>
2016-07-13 19:57:31 -0400

    Update plot2.R

--- figure/plot2.R
+++ figure/plot2.R
@@ -1,10 +1,10 @@
-data$Date <- as.Date(data$Date, format = '%d/%m/%Y')
-data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
-# open device
-if(!file.exists('figures')) dir.create('figures')
-png(filename = './figures/plot2.png', width = 480, height = 480, units='px')
-# plot figure
-Sys.setlocale(category = "LC_ALL", locale = "english")
-plot(data$DateTime, data$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
-# close device
+dataFile <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
