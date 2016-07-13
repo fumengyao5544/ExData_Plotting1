29452ff873c12b971dd9d7257a51d1750a57a773
fumengyao5544 <fumengyao5544@gmail.com>
2016-07-13 19:57:07 -0400

    Update plot1.R

--- figure/plot1.R
+++ figure/plot1.R
@@ -5,15 +5,16 @@ fileUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_powe
 download.file(fileUrl, destfile = './data/household_power_consumption.zip')
 unzip('./data/household_power_consumption.zip', exdir = './data')
 
-# read data into R
-files <- file('./data/household_power_consumption.txt')
-data <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value=TRUE), sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
+
 
 # Plot 1
-# open device
-if(!file.exists('figures')) dir.create('figures')
-png(filename = './figures/plot1.png', width = 480, height = 480, units='px')
-# plot figure
-with(data, hist(Global_active_power, xlab = 'Global Active Power (kilowatt)', ylab = 'Frequency', main = 'Global Active Power', col = 'red'))
-# close device
+# read data into R
+dataFile <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
